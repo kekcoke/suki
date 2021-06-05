19d0ab7625c45534aa301856f184f9ffe895d0f0
kekcoke <kevin.oane@yahoo.com>
2021-06-04 23:26:58 -0700

    #2. Wrap app inside BrowserRouter to interface with routing + Wrap Homepage with added MainLayout

--- src/App.js
+++ src/App.js
@@ -1,15 +1,26 @@
-import { React } from 'react';
-import Header from './components/Header';
+import React from 'react';
+import { Switch, Route } from 'react-router-dom';
+
+// layouts 
+import MainLayout from './layouts/MainLayout';
+import HomepageLayout from './layouts/HomepageLayout';
+
+// pages
 import Homepage from './pages/Homepage';
+import Registration from './pages/Registration';
 import './default.scss';
 
 function App() {
   return (
     <div className="App">
-      <Header/>
-      <div className="main">
-        <Homepage/>
-      </div>
+      <Switch>
+        <Route exact path="/" render={() => (
+          <MainLayout>
+            <Homepage />
+          </MainLayout>
+        )}/>
+        <Route path="/registration" component={Registration} />
+      </Switch>
     </div>
   );
 }
