bb18a98b836cd13a791a4b7298fe6a545de7a931
kekcoke <kevin.oane@yahoo.com>
2021-06-05 09:59:43 -0700

    #3. Extend App with Component + add hooks

--- src/App.js
+++ src/App.js
@@ -1,5 +1,6 @@
-import React from 'react';
+import React, { Component } from 'react';
 import { Switch, Route } from 'react-router-dom';
+import { auth } from './firebase/utils';
 
 // layouts 
 import MainLayout from './layouts/MainLayout';
@@ -13,28 +14,55 @@ import Login from './pages/Login';
 // global styles
 import './default.scss';
 
-function App() {
-  return (
-    <div className="App">
-      <Switch>
-        <Route exact path="/" render={() => (
-          <HomepageLayout>
-            <Homepage />
-          </HomepageLayout>
-        )}/>
-        <Route path="/signup" render={() => (
-          <MainLayout>
-            <Signup />
-          </MainLayout>
-        )}/>
-        <Route path="/login" render={() => (
-          <MainLayout>
-            <Login />
-          </MainLayout>
-        )}/>
-      </Switch>
-    </div>
-  );
+const initialState = {
+  currentUser: null
+}
+
+class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = { ...initialState };
+  }
+
+  authListener = null;
+
+  componentDidMount() {
+    this.auth = auth.onAuthStateChanged(userAuth => {
+      if (!userAuth) return;
+
+      this.setState({
+        currentUser: userAuth
+      });
+    });
+  }
+
+  componentWillUnmount() {
+    this.authListener();
+  }
+
+  render() {
+    return (
+      <div className="App">
+        <Switch>
+          <Route exact path="/" render={() => (
+            <HomepageLayout>
+              <Homepage />
+            </HomepageLayout>
+          )}/>
+          <Route path="/signup" render={() => (
+            <MainLayout>
+              <Signup />
+            </MainLayout>
+          )}/>
+          <Route path="/login" render={() => (
+            <MainLayout>
+              <Login />
+            </MainLayout>
+          )}/>
+        </Switch>
+      </div>
+    );
+  }
 }
 
 export default App;
