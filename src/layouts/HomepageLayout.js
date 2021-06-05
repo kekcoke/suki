e576db79ed704c70545d235825d67bdedd29a3b0
kekcoke <kevin.oane@yahoo.com>
2021-06-04 23:39:45 -0700

    #2. Create HomepageLayout. Remove class attrib fullHeight on MainLayout

--- src/layouts/HomepageLayout.js
+++ src/layouts/HomepageLayout.js
@@ -3,7 +3,7 @@ import Header from './../components/Header';
 
 const MainLayout = props => {
     return (
-        <div>
+        <div className="fullHeight">
             <Header />
             <div className="main">
                 {props.children}
