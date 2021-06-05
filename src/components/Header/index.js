4be273f1cefbaa8193c80e12bb89a0dcaf13b010
kekcoke <kevin.oane@yahoo.com>
2021-06-05 00:25:46 -0700

    #2. Add .callToActions 'signup' top-right

--- src/components/Header/index.js
+++ src/components/Header/index.js
@@ -1,12 +1,24 @@
 import React from 'react';
 import './styles.scss';
 import Logo from './../../assets/logo.png';
+import { Link } from 'react-router-dom';
+
 const Header = props => {
     return (
         <header className="header">
             <div className="wrap">
                 <div className="logo">
+                    <Link to="/" />
                     <img src={Logo} alt="SukiLogo"></img>
+                </div>
+                <div className="callToActions">
+                    <ul>
+                        <li>
+                            <Link to="/signup">
+                                Signup
+                            </Link>
+                        </li>
+                    </ul>
                 </div>
             </div>
         </header>
