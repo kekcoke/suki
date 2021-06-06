a8faacf930b9f0a04f860e1c077003463b70749f
kekcoke <kevin.oane@yahoo.com>
2021-06-06 14:23:00 -0700

    #. scaffold recovery page

---
+++ src/pages/Recovery/index.js
@@ -0,0 +1,12 @@
+import React from 'react';
+import './styles.scss';
+
+const Recovery = props => {
+    return (
+        <div>
+            <h1>Email Password</h1>
+        </div>
+    );
+}
+
+export default Recovery;
