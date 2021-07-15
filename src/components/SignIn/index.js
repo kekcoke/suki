2136807dbfec6de5daa5bb23d15b00402e4ea00b
kekcoke <kevin.oane@yahoo.com>
2021-07-15 09:20:05 -0700

    #8. use redux-hooks, with usedDispatch to call userAction func. use redux-store to map state via mapState and extract signInSuccess

--- src/components/SignIn/index.js
+++ src/components/SignIn/index.js
@@ -1,31 +1,39 @@
-import React, { useState } from 'react';
+import React, { useState, useEffect } from 'react';
+import { useDispatch, useSelector } from 'react-redux';
 import './styles.scss';
 import Button from './../../components/Forms/Button';
 import FormInput from './../Forms/FormInput';
 import AuthWrapper from './../AuthWrapper';
+import { signInUser } from './../../redux/User/user.actions';
 
-import { signInWithGoogle, auth } from './../../firebase/utils';
+import { signInWithGoogle } from './../../firebase/utils';
 import { Link, withRouter } from 'react-router-dom';
 
+const mapState = ({ user }) => ({
+    signInSuccess: user.signInSuccess
+})
+
 const SignIn = props => {
+    const { signInSuccess } = useSelector(mapState);
+    const dispatch = useDispatch();
     const [email, setEmail] = useState('');
     const [password, setPassword] = useState('');
+
+    useEffect(() => {
+        if (signInSuccess) {
+            resetForm();
+            props.history.push('/');
+        }
+    }, [signInSuccess]);
 
     const resetForm = () => {
         setEmail('');
         setPassword('');
     };
 
-    const handleSubmit = async e => {
+    const handleSubmit = e => {
         e.preventDefault();
-
-        try {
-            await auth.signInWithEmailAndPassword(email, password);
-            resetForm();
-            props.history.push('/')
-        } catch (e) {
-            console.error(e);
-        }
+        dispatch(signInUser({ email, password }));
     }
 
     const configAuthWrapper = {
