e0566dc62de5b6d024458efcd4a5795837d0e34c
kekcoke <kevin.oane@yahoo.com>
2021-07-15 09:18:17 -0700

    #8. add notes + signInUser, calling auth request and bundling state

--- src/redux/User/user.actions.js
+++ src/redux/User/user.actions.js
@@ -1,6 +1,31 @@
 import userTypes from './user.types';
+import { auth } from './../../firebase/utils';
 
+/**
+ * Set user state
+ * @param user
+ } user 
+ * @returns typed user object
+ */
 export const setCurrentUser = user => ({
     type: userTypes.SET_CURRENT_USER,
     payload: user
 });
+
+
+/**
+ * Authenticates user
+ * @param {*} object 
+ * @returns auth response
+ */
+export const signInUser = ({ email, password }) => async dispatch => {
+    try {
+        await auth.signInWithEmailAndPassword(email, password);
+        dispatch({
+            type: userTypes.SIGN_IN_SUCCESS,
+            payload: true
+        });
+    } catch (e) {
+        console.error(e);
+    }
+}
