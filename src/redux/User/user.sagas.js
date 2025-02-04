7434923e3dc52ab188cef7f18616916b14e7e16c
Kevin <kolioane@gmail.com>
2025-02-04 14:28:44 -0800

    #9. Fix: Add call, all imports. Pass userSagas to rootSaga

--- src/redux/User/user.sagas.js
+++ src/redux/User/user.sagas.js
@@ -1,4 +1,4 @@
-import { takeLatest } from "redux-saga/effects";
+import { all, call, takeLatest } from "redux-saga/effects";
 import userTypes from "./user.types";
 
 export function* emailSignIn({ payload: { email, password } }) {
@@ -7,4 +7,8 @@ export function* emailSignIn({ payload: { email, password } }) {
 
 export function* onEmailSignInStart() {
   yield takeLatest(userTypes.EMAIL_SIGN_IN_START, emailSignIn);
+}
+
+export default function* userSagas() {
+    yield all([call(onEmailSignInStart)]);
 }
