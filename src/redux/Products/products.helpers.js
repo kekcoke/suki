79203261dda9adb89a2ff84dfaf7c4dbe44652da
Kevin <kolioane@gmail.com>
2025-02-19 15:09:49 -0800

    Add and fetch helper calls #12

--- src/redux/Products/products.helpers.js
+++ src/redux/Products/products.helpers.js
@@ -1,17 +1,32 @@
 import { firestore } from "../../firebase/utils";
 
+const PRODUCTS = "products";
+
 export const handleAddProduct = product => {
     return new Promise((resolve, reject) => {
         firestore
-            .collection("products")
+            .collection(PRODUCTS)
             .doc()
             .set(product)
-            .then(() => {
-                resolve();
-            })
-            .catch(err => {
-                reject(err);
-            });
+            .then(() => resolve())
+            .catch(e => reject(e));
     
     })
 };
+
+export const handleFetchProducts = tindaId => {
+    return new Promise((resolve, reject) => {
+        firestore
+            .collection(PRODUCTS)
+            .get()
+            .then(snapshot => {
+                const productsArray = snapshot.docs.map(doc => {                        return {
+                            ...doc.data(),
+                            documentID: doc.id
+                        }
+                    });
+                    resolve(productsArray);
+                })
+            .catch(e => reject(e));
+    });
+}
