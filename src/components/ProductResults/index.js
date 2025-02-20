2669908d7647a07eec4ea5674b1b37bfb0050678
Kevin <kolioane@gmail.com>
2025-02-20 15:34:52 -0800

    #13. Add empty product handling

--- src/components/ProductResults/index.js
+++ src/components/ProductResults/index.js
@@ -1,6 +1,31 @@
-import React from "react";
+import React, { useEffect } from "react";
+import { useDispatch } from "react-redux";
+import { fetchProductsStart } from "../../redux/Products/products.actions";
+import './styles.scss';
+
+const mapState = ({ productsData }) => ({
+    products: productsData.products
+});
 
 const ProductResults = ({}) => {
+    const dispatch = useDispatch();
+    const { products } = useSelector(mapState);
+
+    useEffect(() => {
+        dispatch(fetchProductsStart());
+    }, []);
+
+    if (!Array.isArray(products)) return null;
+    if (products.length < 1) {
+        return (
+            <div className="products">
+                <p>
+                    No search results.
+                </p>
+            </div>
+        );
+    }
+
     return (
         <div>
             ProductResults
