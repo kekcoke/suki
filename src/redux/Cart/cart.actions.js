48719fe07ae15b3f89c40be98d05ee598542df7f
Kevin <kolioane@gmail.com>
2025-02-25 10:45:42 -0800

    #16. actions and types cart init

---
+++ src/redux/Cart/cart.actions.js
@@ -0,0 +1,11 @@
+import cartTypes from "./cart.types";
+
+export const addProduct = (nextCartItem) => ({
+    type: cartTypes.ADD_TO_CART,
+    payload: nextCartItem
+});
+
+export const removeCartItem = (cartItem) => ({
+    type: cartTypes.REMOVE_CART_ITEM,
+    payload: cartItemID
+});
