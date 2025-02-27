cdea0db56f79f32f050aae1476b225bc7f68d336
Kevin <kolioane@gmail.com>
2025-02-27 08:25:41 -0800

    #18 add and config redux-persist for cartData

--- src/redux/createStore.js
+++ src/redux/createStore.js
@@ -1,15 +1,18 @@
-import { applyMiddleware, createStore } from 'redux';
-import logger from 'redux-logger';
-import createSagaMiddleware from 'redux-saga';
-import { thunk } from 'redux-thunk';
+import { applyMiddleware, createStore } from "redux";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { thunk } from "redux-thunk";
 
-import rootReducer from './rootReducer';
-import rootSaga from './rootSaga';
+import persistStore from "redux-persist/es/persistStore";
+import rootReducer from "./rootReducer";
+import rootSaga from "./rootSaga";
 
 const sagaMiddleware = createSagaMiddleware();
 export const middlewares = [thunk, sagaMiddleware, logger];
 
 export const store = createStore(rootReducer, applyMiddleware(...middlewares));
 sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
 
 export default store;
--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -1,11 +1,21 @@
-import { combineReducers } from 'redux';
+import { combineReducers } from "redux";
 
-import cartReducer from './Cart/cart.reducer';
-import productsReducer from './Products/products.reducer';
-import userReducer from './User/user.reducer';
+import { persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import cartReducer from "./Cart/cart.reducer";
+import productsReducer from "./Products/products.reducer";
+import userReducer from "./User/user.reducer";
 
-export default combineReducers({
-    user: userReducer,
-    productsData: productsReducer,
-    cartData: cartReducer
+export const rootReducer = combineReducers({
+  user: userReducer,
+  productsData: productsReducer,
+  cartData: cartReducer,
 });
+
+const configPersistStorage = {
+  key: "root",
+  storage,
+  whiteList: ["cartData"],
+};
+
+export default persistReducer(configPersistStorage, rootReducer);
