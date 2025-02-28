3e6fc14e3c81d807791f4ec5b948ad97feddeb5f
Kevin <kolioane@gmail.com>
2025-02-28 13:19:37 -0800

    #21. Orders rootReducer + actions + reducer + types init

--- src/redux/rootReducer.js
+++ src/redux/rootReducer.js
@@ -3,6 +3,7 @@ import { combineReducers } from "redux";
 import { persistReducer } from "redux-persist";
 import storage from "redux-persist/lib/storage";
 import cartReducer from "./Cart/cart.reducer";
+import ordersReducer from "./Orders/orders.reducer";
 import productsReducer from "./Products/products.reducer";
 import userReducer from "./User/user.reducer";
 
@@ -10,6 +11,7 @@ export const rootReducer = combineReducers({
   user: userReducer,
   productsData: productsReducer,
   cartData: cartReducer,
+  ordersData: ordersReducer,
 });
 
 const configPersistStorage = {
